b3b584da9d8e746d484bed95e194d0ab4e520e36
ronin769 <1471656192@qq.com>
2020-12-28 05:05:11 +0800

    20201228初始化更新

---
+++ config.js
@@ -0,0 +1,44 @@
+/*
+ * @Description: 页面基本配置
+ * @Author: jiangyihong
+ * @Date: 2020-08-10 09:22:25
+ * @LastEditors: yaolin
+ */
+// 接口地址
+window.BASEPATH = 'https://hdlyt.linker.cc/app/sqt/api/' // 袁作成
+// window.BASEPATH = 'http://192.1.2.79:8098/' // 袁作成
+// window.BASEPATH = 'http://192.1.2.7:8099/' // xinzheng
+// window.BASEPATH = 'http://192.1.2.21:5009/' // xinzheng
+// window.BASEPATH = 'http://192.1.2.59:8098/' // 余海
+window.CONSULTPATH = 'https://hdlyt.linker.cc/app/yyxx/api' // 王文春
+// 信箱接口
+window.MAILBASEPATH = 'http://192.1.2.21:5015/api/' // 春哥
+
+
+// 应用底座接口地址
+// window.APPBASEAPI = 'http://192.1.2.58:8088/' // 刘志强
+// window.APPBASEAPI = 'http://192.1.2.38:8088/' // 吴杭峰
+window.APPBASEAPI = 'http://192.1.2.144:8088/' // 袁作成
+// window.APPBASEAPI = 'http://192.1.2.75:8088/' // 吴亮
+
+
+// 基础底座接口API地址
+// window.BASEAPI = 'http://192.1.2.58:8090/' // 刘志强
+// window.BASEAPI = 'http://192.1.2.214:8000/base/api2/'
+// window.BASEAPI = 'http://192.1.2.38:8090' // 吴杭峰
+window.BASEAPI = 'http://192.1.2.144:8090/' // 袁作成
+// window.BASEAPI = 'http://192.1.2.75:8090/' // 吴亮
+
+// OIDC地址
+// window.OIDCPATH = 'http://192.1.2.214:8000/auth/'
+window.OIDCPATH = 'http://192.1.2.62:8080/auth/'
+// OIDC ClientID
+window.CLIENTID = 'yaolin'
+// 领域名称
+window.REALM = 'lyt'
+
+
+// 融媒选人组件地址
+window.ASSPATH = 'http://192.1.3.54:8000/app/sltusr/'
+// 富文本编辑器地址
+window.EDITORPATH='http://192.1.2.172:12345/Editor_Assembly/'+'?time='+new Date().getTime()
